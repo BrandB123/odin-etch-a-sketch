7c19629aab265a3159a0ec5df3450c34fc7d7454
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-05-03 22:32:40 -0500

    Implement default grid
    I added a container to index.html for the grid to be created within.
    
    In script.js I implemented a function that would allow me to create a grid
    of a given number of divs using DOM methods.
    
    In style.css I added flexbox to each div to place it in the desired location
    on the page.

---
+++ script.js
@@ -0,0 +1,29 @@
+// default grid size
+let gridWidth = 16;
+let gridHeight = 16;
+
+let container = document.querySelector(".grid-container");
+
+function createGrid(width, height){
+    //clear div-container
+    while (container.firstChild) {
+        container.remove(container.firstChild);
+    }
+    //fill div-container
+    for (let i = 0; i < height; i++) {
+        let divRow = document.createElement("div");
+        divRow.setAttribute("class", "div-row");
+	    divRow.setAttribute("id", `div-row${i}`);
+	    container.appendChild(divRow);
+        for (let j = 0; j < width; j++) {
+            let divColumn = document.createElement("div");
+            divColumn.setAttribute("class", "div-column");
+	        divColumn.setAttribute("id", `div-column${i}`);
+	        divRow.appendChild(divColumn);
+        } 
+    }
+}
+
+// create default grid
+createGrid(gridWidth, gridHeight);
+
