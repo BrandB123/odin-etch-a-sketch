1a57b411062ce9684b3e7a7d8537b44691d42e31
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-05-03 23:43:56 -0500

    Add random coloring & increasing opacity in grid
    I implemented two functions that can be called in the mouseover
    eventListener to determine if the color of the grid is black and white
    or colored.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let gridWidth = 16;
 let gridHeight = 16;
 
 let container = document.querySelector(".grid-container");
+let btn = document.querySelector("button");
+
 
 function createGrid(width, height){
     //clear div-container
@@ -24,18 +26,41 @@ function createGrid(width, height){
     }
 }
 
-// create default grid
-createGrid(gridWidth, gridHeight);
+
+function basicDrawing(target) {
+    target.style.cssText = "background-color: black;";
+}
+
+
+function advancedDrawing(target) {
+    if (target.style.backgroundColor == "") {
+        let red = Math.round(Math.random() * 255);
+        let green = Math.round(Math.random() * 255);
+        let blue = Math.round(Math.random() * 255);
+        target.style.cssText =`background-color: rgb(${red}, ${blue}, ${green});`;
+        target.style.opacity = 0.1;
+    } else {
+        let opacity = target.style.opacity;
+        opacity = Number(opacity) + 0.1;
+        if (opacity > 1){
+            opacity = 1;
+        }
+        let background = target.style.backgroundColor;
+        target.style.cssText = `background-color: ${background}; opacity: ${opacity}`;
+        opacity = target.style.opacity;
+        background = target.style.backgroundColor;
+    }
+}
 
 
 container.addEventListener("mouseover", (event) => {
-    /*let target = event.target.id;*/
     let target = event.target;
-    target.style.cssText = "background-color: black;";
+    //basicDrawing(target);
+    advancedDrawing(target);
 });
 
 
-let btn = document.querySelector("button");
+//let btn = document.querySelector("button");
 btn.addEventListener("click", () => {
     do {
         alert("Width and height must be greater then 10 and less than 100.")
@@ -47,3 +72,6 @@ btn.addEventListener("click", () => {
     createGrid(gridWidth, gridHeight);
 });
 
+
+// create default grid
+createGrid(gridWidth, gridHeight);
